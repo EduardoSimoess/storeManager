303794b9efe1f29bdcf0f76cbae53a62d1f4d959
Eduardo Simões <eduardorsimoes97@gmail.com>
2022-10-12 11:46:52 -0300

    Começo dos testes

---
+++ tests/unit/models/products.test.js
@@ -0,0 +1,15 @@
+// const { expect } = require('chai');
+
+// const sinon = require('sinon');
+
+// const passagerModel = require('../../../src/models/product.model');
+
+// const connection = require('../../../src/models/connection');
+
+// describe('Testes unitários para a busca de produtos', function () {
+//   afterEach(sinon.restore);
+
+//   it('Recuperando a lista de produtos', async function () {
+//     sinon.stub(connection, 'execute').
+//   });
+// });
